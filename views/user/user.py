30e9c5ac99d44486ff48a1007906dad2442e75c8
kpt <2817151932@qq.com>
2024-07-02 17:09:39 +0800

    【user.py】添加注册接口

--- views/user/user.py
+++ views/user/user.py
@@ -1,3 +1,4 @@
+import time
 from flask import Blueprint, redirect, render_template, request,Flask, session
 
 from utils.query import query
@@ -19,3 +20,24 @@ def login():
 
         session['username'] = request.form['username']
         return redirect('/page/home')
+    
+@ub.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        if request.form['password'] != request.form['checkPassword']:return errorResponse('两次密码不符合')
+        def filter_fn(user):
+            return request.form['username'] in user
+
+        users = query('select * from user',[],'select')
+        filter_list = list(filter(filter_fn,users))
+        if len(filter_list):
+            return errorResponse('该用户名已被注册')
+        else:
+            time_tuple = time.localtime(time.time())
+            query('''
+                insert into user(username,password,createTime) values(%s,%s,%s)
+            ''',[request.form['username'],request.form['password'],str(time_tuple[0]) + '-' + str(time_tuple[1]) + '-' + str(time_tuple[2])])
+
+        return redirect('/user/login')
