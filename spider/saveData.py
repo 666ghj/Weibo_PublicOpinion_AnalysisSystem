c0686430de4b830d68ec84b23e3e208b8127761e
YYL469 <2049360881@qq.com>
2024-07-03 21:55:16 +0800

    增加spider脚本，每5小时爬取一次数据

--- spider/saveData.py
+++ spider/saveData.py
@@ -6,24 +6,24 @@ engine = create_engine('mysql+pymysql://XiaoXueQi:XiaoXueQi@10.92.35.13/Weibo_Pu
 
 def save_to_sql():
     try:
-        artileOldPd = pd.read_sql('select * from article',engine)
-        articleNewPd = pd.read_csv('article.csv')
-        commentOldPd = pd.read_sql('select * from comments',engine)
-        commentNewPd = pd.read_csv('comments.csv')
+        oldArticle = pd.read_sql('select * from article',engine)
+        newArticle = pd.read_csv('article.csv')
+        oldComment = pd.read_sql('select * from comments',engine)
+        newComment = pd.read_csv('comments.csv')
 
-        concatArticlePd = pd.concat([articleNewPd,artileOldPd],join='inner')
-        concatCommentsPd = pd.concat([commentNewPd,commentOldPd],join='inner')
+        mergeArticle = pd.concat([newArticle,oldArticle],join='inner')
+        mergeComment = pd.concat([newComment,oldComment],join='inner')
 
-        concatArticlePd.drop_duplicates(subset='id',keep='last',inplace=True)
-        concatCommentsPd.drop_duplicates(subset='content',keep='last',inplace=True)
+        mergeArticle.drop_duplicates(subset='id',keep='last',inplace=True)
+        mergeComment.drop_duplicates(subset='content',keep='last',inplace=True)
 
-        concatArticlePd.to_sql('article', con=engine, if_exists='replace', index=False)
-        concatCommentsPd.to_sql('comments', con=engine, if_exists='replace', index=False)
+        mergeArticle.to_sql('article', con=engine, if_exists='replace', index=False)
+        mergeComment.to_sql('comments', con=engine, if_exists='replace', index=False)
     except:
-        articleNewPd = pd.read_csv('article.csv')
-        commentNewPd = pd.read_csv('comments.csv')
-        articleNewPd.to_sql('article',con=engine,if_exists='replace',index=False)
-        commentNewPd.to_sql('comments',con=engine,if_exists='replace',index=False)
+        newArticle = pd.read_csv('article.csv')
+        newComment = pd.read_csv('comments.csv')
+        newArticle.to_sql('article',con=engine,if_exists='replace',index=False)
+        newComment.to_sql('comments',con=engine,if_exists='replace',index=False)
 
     os.remove('./article.csv')
     os.remove('./comments.csv')
--- spider/spiderNav.py
+++ spider/spiderNav.py
@@ -13,12 +13,12 @@ def init():
                 'containerid'
             ])
 
-def writerRow(row):
+def write(row):
     with open('./nav.csv', 'a', encoding='utf-8', newline='') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerow(row)
 
-def get_data(url):
+def fetchData(url):
     headers = {
         'Cookie':'SINAGLOBAL=2555941826014.1074.1676801766625; ULV=1719829459275:6:1:2:4660996305989.918.1719827559898:1719743122299; UOR=,,www.baidu.com; XSRF-TOKEN=VtLXviYSIs8lor7sz4iGyigL; SUB=_2A25LhvU9DeRhGeFH6FIX-S3MyD2IHXVo-gj1rDV8PUJbkNAGLRXMkW1Ne2nhI3Gle25QJK0Z99J3trq_NZn6YKJ-; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WW3Mv8V5EupQbbKh.vaZIwU5JpX5KzhUgL.FoM4e05c1Ke7e022dJLoIp7LxKML1KBLBKnLxKqL1hnLBoM41hz41hqReKqN; WBPSESS=Dt2hbAUaXfkVprjyrAZT_LRaDLsnxG-kIbeYwnBb5OUKZiwfVr_UrcYfWuqG-4ZVDM5HeU3HXkDNK_thfRfdS9Ao6ezT30jDksv-CpaVmlTAqGUHjJ7PYkH5aCK4HLxmRq14ZalmQNwzfWMPa4y0VNRLuYdg7L1s49ymNq_5v5vusoz0r4ki6u-MHGraF0fbUTgX14x0kHayEwOoxfLI-w==; SCF=AqmJWo31oFV5itnRgWNU1-wHQTL6PmkBLf3gDuqpdqAIfaWguDTMre6Oxjf5Uzs74JAh2r0DdV1sJ1g6m-wJ5NQ.; _s_tentry=-; Apache=4660996305989.918.1719827559898; PC_TOKEN=7955a7ab1f; appkey=; geetest_token=602cd4e3a7ed1898808f8adfe1a2048b; ALF=1722421868',
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0'
@@ -33,13 +33,13 @@ def get_data(url):
     else:
         return None
 
-def parse_json(response):
+def readJson(response):
     navList = np.append(response['groups'][3]['group'],response['groups'][4]['group'])
     for nav in navList:
         navName = nav['title']
         gid = nav['gid']
         containerid = nav['containerid']
-        writerRow([
+        write([
             navName,
             gid,
             containerid
@@ -49,5 +49,5 @@ def parse_json(response):
 if __name__ == '__main__':
     init()
     url = 'https://weibo.com/ajax/feed/allGroups'
-    response = get_data(url)
-    parse_json(response)
+    response = fetchData(url)
+    readJson(response)
